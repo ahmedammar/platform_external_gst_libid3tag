b03d4dbb252264d098b5bea8a7377ff59037a4f0
Alessandro Decina <alessandro.d@gmail.com>
2011-03-02 15:08:30 +0100

    android: Rework the way built sources are generated

--- Android.mk
+++ Android.mk
@@ -8,7 +8,7 @@ id3tag_TOP := $(LOCAL_PATH)
 ID3TAG_BUILT_SOURCES := 		\
 	libid3tag-Android.mk
 
-ABS_ID3TAG_BUILT_SOURCES := $(patsubst %, $(id3tag_TOP)/%, $(ID3TAG_BUILT_SOURCES))
+ID3TAG_BUILT_SOURCES := $(patsubst %, $(id3tag_TOP)/%, $(ID3TAG_BUILT_SOURCES))
 
 .PHONY: id3tag-configure
 id3tag-configure-real:
@@ -24,13 +24,12 @@ id3tag-configure-real:
 	PKG_CONFIG_LIBDIR=$(CONFIGURE_PKG_CONFIG_LIBDIR) \
 	PKG_CONFIG_TOP_BUILD_DIR=/ \
 	$(id3tag_TOP)/configure --host=arm-linux-androideabi \
-	--prefix=/system
+	--prefix=/system ; \
+	for file in $(ID3TAG_BUILT_SOURCES); do \
+		make -C $$(dirname $$file) $$(basename $$file) ; \
+	done
 
-.PHONY: $(ABS_ID3TAG_BUILT_SOURCES)
-$(ABS_ID3TAG_BUILT_SOURCES):
-	make -C $$(dirname $@) $$(basename $@)
-
-id3tag-configure: id3tag-configure-real $(ABS_ID3TAG_BUILT_SOURCES)
+id3tag-configure: id3tag-configure-real
 
 CONFIGURE_TARGETS += id3tag-configure
 
