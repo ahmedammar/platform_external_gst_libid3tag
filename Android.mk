b8334967877fbbde0917642942d4b5422a3a20e2
Alessandro Decina <alessandro.d@gmail.com>
2011-03-04 12:02:47 +0100

    Use absolute paths to run configure and built sources

--- Android.mk
+++ Android.mk
@@ -8,7 +8,7 @@ id3tag_TOP := $(LOCAL_PATH)
 ID3TAG_BUILT_SOURCES := 		\
 	libid3tag-Android.mk
 
-ID3TAG_BUILT_SOURCES := $(patsubst %, $(id3tag_TOP)/%, $(ID3TAG_BUILT_SOURCES))
+ID3TAG_BUILT_SOURCES := $(patsubst %, $(abspath $(id3tag_TOP))/%, $(ID3TAG_BUILT_SOURCES))
 
 .PHONY: id3tag-configure
 id3tag-configure-real:
@@ -23,8 +23,8 @@ id3tag-configure-real:
 	CPPFLAGS="$(CONFIGURE_CPPFLAGS)" \
 	PKG_CONFIG_LIBDIR=$(CONFIGURE_PKG_CONFIG_LIBDIR) \
 	PKG_CONFIG_TOP_BUILD_DIR=/ \
-	$(id3tag_TOP)/configure --host=arm-linux-androideabi \
-	--prefix=/system ; \
+	$(abspath $(id3tag_TOP))/configure --host=arm-linux-androideabi \
+	--prefix=/system && \
 	for file in $(ID3TAG_BUILT_SOURCES); do \
 		make -C $$(dirname $$file) $$(basename $$file) ; \
 	done
