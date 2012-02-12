541c222a134b08c4b0ee54e09c29d7964433af55
Ahmed Ammar <aammar@genesi-usa.com>
2012-02-12 13:37:36 +0200

    Update for ICS.

--- Android.mk
+++ Android.mk
@@ -27,7 +27,7 @@ libid3tag-configure: $(TARGET_CRTBEGIN_DYNAMIC_O) $(TARGET_CRTEND_O) $(LIB)/libc
 	LDFLAGS="$(CONFIGURE_LDFLAGS)" \
 	CPP=$(CONFIGURE_CPP) \
 	CPPFLAGS="$(CONFIGURE_CPPFLAGS)" \
-	PKG_CONFIG_LIBDIR=$(CONFIGURE_PKG_CONFIG_LIBDIR) \
+	PKG_CONFIG_PATH=$(CONFIGURE_PKG_CONFIG_PATH) \
 	PKG_CONFIG_TOP_BUILD_DIR=/ \
 	$(abspath $(id3tag_TOP))/configure --host=arm-linux-androideabi \
 	--prefix=/system && \
